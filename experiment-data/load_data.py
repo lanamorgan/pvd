40c4a10c270ece3a1e78866e5e1cce471865f07e
lana <alanamramjit@gmail.com>
2021-10-04 11:45:28 -0700

    remove extra column

--- experiment-data/load_data.py
+++ experiment-data/load_data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
 
-data = "./csvs/"
+data = "./csvs"
 load_csv_names = [f for f in listdir(data) if isfile(join(data, f)) and f.endswith(".csv")]
 csv_as_df = [pd.read_csv(join(data, csv)) for csv in load_csv_names]
 conn = sql.connect('pvd.db')
